ef2272693d6e3e2d07e99d037bd764f1de4f2741
code-forcer <135433507+code-forcer@users.noreply.github.com>
2025-03-10 19:28:27 +0100

    Update Volunteer.js

--- model/Volunteer.js
+++ model/Volunteer.js
@@ -1,10 +1,15 @@
-// models/Volunteer.js
 const mongoose = require("mongoose");
 
 const volunteerSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  message: { type: String, required: true },
+  phone: { type: String, required: true },
+  address: { type: String, required: true },
+  reason: { type: String, required: true },
+  skills: { type: String, required: true },
+  team: { type: String, required: true },
+  hoursPerWeek: { type: Number, required: true },
+  availability: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
